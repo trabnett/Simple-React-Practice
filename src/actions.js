c9e5454214b8c0bee5b02fabd9fb186a82e80444
trabnett <timrabnett@gmail.com>
2019-03-19 09:14:04 -0400

    set up redux store. Comment state still stored in App component

---
+++ src/actions.js
@@ -0,0 +1,4 @@
+import { ADD_ARTICLE } from "./constants";
+export function addArticle(payload) {
+  return { type: ADD_ARTICLE, payload };
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from "react-dom";
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from "./store";
+import { addArticle } from "./actions";
+window.store = store;
+window.addArticle = addArticle;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
